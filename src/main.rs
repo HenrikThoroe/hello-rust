128e2d199bbe05e71fb2041db05371303304b438
Henrik Thoroe <henrik@thoroee.com>
2020-07-03 15:32:41 +0200

    Clean

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 extern crate clap;
 use clap::{Arg, App};
 
-mod math;
-use math::pow as pow;
-use math::mult as mult;
+// mod math;
+// use math::pow as pow;
+// use math::mult as mult;
 
 fn main() {
-    let app = App::new("Hello Rust").arg(Arg::with_name("input").long("input").takes_value(true));
+    let app = App::new("Hello Rust")
+                .arg(Arg::with_name("input")
+                    .long("input")
+                    .takes_value(true)
+                    .required(true));
 
     let matches = app.get_matches();
+    let input = matches.value_of("input").unwrap();
 
-    let input = matches.value_of("input").unwrap_or("Nothing Provided");
     println!("{}", input);
-
-    let power = pow(&2, &3);
-    let prod = mult(&2, &3);
-    println!("{}, {}", power, prod);
 }
